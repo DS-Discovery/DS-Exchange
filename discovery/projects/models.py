74bbdbf530616de549cd6b52c365815c4cc59ce4
Anderson Lam <anderson.lam14@gmail.com>
2020-11-15 13:47:23 -0800

    changes before merge

--- discovery/projects/models.py
+++ discovery/projects/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Project(models.Model):
     organization = models.CharField(max_length=100)
 
-    project_name = models.CharField(max_length=200, primary_key=True)
+    project_name = models.CharField(max_length=200)
 
     project_category = models.CharField(max_length=100)
     student_num = models.IntegerField(default=0)
@@ -29,6 +29,7 @@ class Partner(models.Model):
 class Question(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
+    # question_type = models.CharField(max_length=200)
     # def create_id(partner, question_text):
     #     return hash(str(partner) + str(question_text))
     #
