b1b3b26890cb6cc056db6ffa4fe1d81b4c87a0fa
Edmond Tu <edmond8tu@berkeley.edu>
2020-11-25 01:30:34 -0800

    migration issue resolved, model changes reflected in local postgres

--- discovery/discovery/settings.py
+++ discovery/discovery/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'projects.apps.ProjectsConfig',
     'students.apps.StudentsConfig',
+    'applications.apps.ApplicationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
