9d49333147535787f680aa99efa9838b914b1e58
Edmond Tu <edmond8tu@berkeley.edu>
2020-11-28 18:52:02 -0800

    changed primary key of application model to id because a student may have many applications (first, second, third choices)

--- discovery/seed_applications.py
+++ discovery/seed_applications.py
@@ -30,14 +30,24 @@ if __name__ == "__main__":
         choices.append(row["3) What is your THIRD choice?"])
 
         for proj in choices:
+
+
             try:
                 projObj = Project.objects.get(project_name=proj)
                 proj_id = projObj.id
+
+
 
                 application = Application(email_address=row["Email Address"],
                                           project_id=proj_id,
                                           status="SENT",
                                           )
+
+                if row["Email Address"] == "yasinifatema@berkeley.edu":
+                    print(proj)
+                    print(proj_id)
+                    print(application)
+
                 application.save()
 
             except ObjectDoesNotExist:
