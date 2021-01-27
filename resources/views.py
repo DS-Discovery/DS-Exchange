697c7141da3fc2ab157b7d9fb43f599f439f09d9
Eunice Chan <ekchan@berkeley.edu>
2021-01-26 23:38:19 -0800

    Changed resources page rendering method

--- resources/views.py
+++ resources/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    content = "<i>load</i> text here!"
-    return render(request, 'one_column_view.html', {"content": content})
+    return render(request, 'resources/resources.html')
