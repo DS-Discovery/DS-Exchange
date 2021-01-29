3118b106ddd694632345b94ae9251e62f75b01fd
Kashish <kashishkhar@gmail.com>
2021-01-28 21:37:44 -0800

    team roster tab
    started the team roster app

--- discovery/urls.py
+++ discovery/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('applications/', include('applications.urls')),
     path('profile/', include('user_profile.urls')),
     path('projects/', include('projects.urls')),
+    path('roster/', include('roster.urls')),
 ]
 
 handler400 = status_400 #'discovery.views.status_403'
