23d91d04a33fc7d5861be97468f8b6a347f2f2b4
Eunice Chan <ekchan@berkeley.edu>
2021-02-19 17:30:54 -0800

    skeleton status summary + staff auth up

--- discovery/admin.py
+++ discovery/admin.py
@@ -1,29 +1,34 @@
 from django.contrib.admin import AdminSite
 from django.conf.urls import url
 from django.template.response import TemplateResponse
+from django.contrib.admin.views.decorators import staff_member_required
 
 class DiscoveryAdmin(AdminSite):
     def get_urls(self):
         urls = super(DiscoveryAdmin, self).get_urls()
         additional_urls = [
-            url(r'status_summary/', self.status_summary),
-            url(r'overview/', self.overview),
+            url(r'status_summary/', status_summary),
+            url(r'overview/', overview),
         ]
         return additional_urls + urls
 
-    # Custom Views
-    def status_summary(self, request):
-        # ...
-        context = dict(
-           title='Status summary'
-        )
-        return TemplateResponse(request, "admin/status_summary.html", context)
+# Custom Views
+@staff_member_required
+def status_summary(request):
+    # ...
+    context = dict(
+       title='Status summary',
+       has_permission=request.user.is_authenticated,
+    )
+    return TemplateResponse(request, "admin/status_summary.html", context)
 
-    def overview(self, request):
-        # ...
-        context = dict(
-           key='value',
-        )
-        return TemplateResponse(request, "admin/overview.html", context)
+@staff_member_required
+def overview(request):
+    # ...
+    context = dict(
+       title='Overview',
+       has_permission=request.user.is_authenticated,
+    )
+    return TemplateResponse(request, "admin/overview.html", context)
 
 admin_site = DiscoveryAdmin(name='discovery_admin')
