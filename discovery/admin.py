8c0ee0a04245b92b9c22400395de0a54becd83d9
Eunice Chan <ekchan@berkeley.edu>
2021-02-19 16:56:50 -0800

    skeleton backend for new pages

--- discovery/admin.py
+++ discovery/admin.py
@@ -1,8 +1,29 @@
-from django.contrib.auth.models import Group, User
-from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib.admin import AdminSite
+from django.conf.urls import url
+from django.template.response import TemplateResponse
 
 class DiscoveryAdmin(AdminSite):
-    pass
+    def get_urls(self):
+        urls = super(DiscoveryAdmin, self).get_urls()
+        additional_urls = [
+            url(r'status_summary/', self.status_summary),
+            url(r'overview/', self.overview),
+        ]
+        return additional_urls + urls
+
+    # Custom Views
+    def status_summary(self, request):
+        # ...
+        context = dict(
+           title='Status summary'
+        )
+        return TemplateResponse(request, "admin/status_summary.html", context)
+
+    def overview(self, request):
+        # ...
+        context = dict(
+           key='value',
+        )
+        return TemplateResponse(request, "admin/overview.html", context)
 
 admin_site = DiscoveryAdmin(name='discovery_admin')
