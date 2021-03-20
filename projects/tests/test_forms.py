6986008323aa07597eea9cf8c7f323401e1d3836
Eunice Chan <ekchan@berkeley.edu>
2021-03-20 00:50:33 -0700

    scaffolding for testing form should it become more complicated

--- projects/tests/test_forms.py
+++ projects/tests/test_forms.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from projects.forms import EditProjectForm
+
+
+class EditProjectFormTestCase(TestCase):
+    # https://adamj.eu/tech/2020/06/15/how-to-unit-test-a-django-form/
+    pass
