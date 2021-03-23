c8c0ed2394f57fe800de8818a0a030af53a11a23
Eunice Chan <ekchan@berkeley.edu>
2021-03-22 23:34:53 -0700

    added space

--- projects/tests/factories/partner.py
+++ projects/tests/factories/partner.py
@@ -3,6 +3,6 @@ import factory
 class PartnerFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'projects.Partner'
-    email_address =factory.Faker('email')
+    email_address = factory.Faker('email')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
