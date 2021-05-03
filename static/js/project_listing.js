ed8d6fadcd0c4ce1f0172dd228d38138d1210bf5
Eunice Chan <ekchan@berkeley.edu>
2021-05-03 15:47:28 -0700

    add semicolons to js

--- static/js/project_listing.js
+++ static/js/project_listing.js
@@ -6,9 +6,9 @@ const categoryFilterSelectId = "category-filter-select";
 const copyButtonId = "copy-link";
 const linkSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="1.25rem"
 style="margin-bottom: 2px; padding-bottom: 5px; margin-left: 5px; opacity: 0.5;">
-    <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 
-    2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 
-    1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 
+    <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5
+    2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25
+    1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95
     0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z">
     </path>
 </svg>`;
@@ -142,8 +142,8 @@ function replaceDescription(project) {
 
     var htmlDescription = mdConverter.makeHtml(project.description);
     var htmlTimeline = mdConverter.makeHtml(project.timeline);
-    var htmlWorkflow = mdConverter.makeHtml(project.project_workflow)
-    var htmlOrgDescription = mdConverter.makeHtml(project.organization_description)
+    var htmlWorkflow = mdConverter.makeHtml(project.project_workflow);
+    var htmlOrgDescription = mdConverter.makeHtml(project.organization_description);
 
     $(descriptionQuery).append(`
         <h5 style="display: inline;">${ project.project_name }</h5>
