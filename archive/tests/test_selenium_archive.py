3081b011c8134a04e4b8b96cda5a8b5aaa023f20
Eunice Chan <ekchan@berkeley.edu>
2021-05-26 08:20:17 -0700

    cleaned up archive tests

--- archive/tests/test_selenium_archive.py
+++ archive/tests/test_selenium_archive.py
@@ -2,18 +2,14 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from django.conf import settings
 
 from projects.tests.factories.project import ProjectFactory
 from applications.tests.factories.application import ApplicationFactory
-from projects.tests.factories.partnerprojectinfo import PartnerProjectInfoFactory
 from projects.models import Semester, Project
 
 import json
 import random
-import time
-import re
 from bs4 import BeautifulSoup
 
 # Create your tests here.
@@ -24,17 +20,20 @@ class ArchiveTest(StaticLiveServerTestCase):
 
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        cls.selenium = webdriver.Chrome(r'C:\Users\eunic\Downloads\chromedriver_win32\chromedriver.exe', options=chrome_options)
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--ignore-ssl-errors=yes")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+        chromedriver = settings.WEBDRIVER
+        cls.selenium = webdriver.Chrome(chromedriver, options=chrome_options)
 
         sem_map = {k:v for k, v in Project.sem_mapping.items()}
         cls.short_current_semester = 'SP21'
         cls.current_semester = sem_map[cls.short_current_semester]
         settings.CONSTANCE_CONFIG['CURRENT_SEMESTER'] = (cls.current_semester, "Current semester", str)
-
-        #cls.selenium.implicitly_wait(5)
-
-        # cls.partner = UserFactory(password=cls.password)
-        # cls.partner_obj = PartnerFactory(email_address=cls.partner.email)
 
         cls.semesters = [s[0] for s in Semester.choices]
         cls.semesterCt = len(cls.semesters)
@@ -44,33 +43,20 @@ class ArchiveTest(StaticLiveServerTestCase):
 
         cls.projectOrganizationStr = "Project Organization: "
 
+    ### START HELPER FUNCTIONS ###
+
     def page_validation(self, projList, appList = None):
 
         self.assertTrue(self.selenium.title == 'Data Science Discovery Program')
 
-        # projCatList = []
         projNameList = []
         optionList = []
 
         selectedProjList = [ x for x in projList if x.semester != self.short_current_semester]
         for proj in selectedProjList:
             projNameList.append(proj.project_name)
-            # for item in proj.project_category.split(";"):
-            #     projCatList.append(item)
 
-        # projCatList = sorted(set(projCatList))
         projNameList = sorted(set(projNameList))
-
-        # categoryFilter = self.selenium.find_element_by_id("category-filter-select")
-        # options = [x for x in categoryFilter.find_elements_by_tag_name("option")]
-        #
-        # for element in options:
-        #     option = element.get_attribute("value")
-        #     if (option != ''):
-        #         optionList.append(option)
-
-        # # validate "Project Category"
-        # self.assertEqual(optionList,projCatList)
 
         # validate "Project List"
         jsonText = self.selenium.find_element_by_id("projects-json").get_attribute("text")
@@ -78,9 +64,9 @@ class ArchiveTest(StaticLiveServerTestCase):
         i = 0
 
         for project in projects_json:
-            self.assertEqual(project['project_name'],projNameList[i])
+            self.assertEqual(project['project_name'], projNameList[i])
             #always return non current semester even the semester is in the future
-            self.assertNotEqual(project['semester'],self.current_semester)
+            self.assertNotEqual(project['semester'], self.current_semester)
 
             # validate the detail page
             project_button = self.selenium.find_element_by_id('project-'+ str(i))
@@ -90,61 +76,14 @@ class ArchiveTest(StaticLiveServerTestCase):
 
             selectedProj = [x for x in selectedProjList if x.project_name == project['project_name']]
 
-            self.assertEqual(len(selectedProj),1)
+            self.assertEqual(len(selectedProj), 1)
             selectedProj = selectedProj[0]
 
             descr_html = BeautifulSoup(self.selenium.find_element_by_id('description').get_attribute('innerHTML'), features="html.parser")
-            self.assertEqual(project['project_name'],descr_html.find("h5").text)
-            self.assertEqual(project['embed_link'],descr_html.find_all('iframe')[0]['src'])
+            self.assertEqual(project['project_name'], descr_html.find("h5").text)
+            self.assertEqual(project['embed_link'], descr_html.find_all('iframe')[0]['src'])
 
-            # ## Skill Set
-            # skill_table = descr_html.find_all("table")
-            # for t in skill_table:
-            #     bkey = True
-            #     for cell in t.find_all("td"):
-            #         if bkey:
-            #             key = cell.text.rstrip()
-            #         else:
-            #             self.assertEqual(project['skillset'][key],cell.text)
-            #         bkey = not bkey
-            #
-            # bMatchNext = False
-            # matchText = ""
-            # for p in descr_html.find_all("p"):
-            #     if (bMatchNext):
-            #         bMatchNext = False
-            #         self.assertEqual(matchText,p.text)
-            #
-            #     if (p.text in self.projMap.keys()):
-            #         bMatchNext = True
-            #         matchText = getattr(selectedProj,self.projMap[p.text])
-            #
-            #     if (p.text.startswith(self.projectOrganizationStr)):
-            #         self.assertEqual(p.text.replace(self.projectOrganizationStr,''),getattr(selectedProj,"organization"))
-            #         ## ?? NEED to Check
-            #         # bMatchText = True
-            #         # matchText = getattr(selectedProj[0],"organization_description")
-            #
-            # # check for project-sidebar
-            # projectSidebar_html = BeautifulSoup(self.selenium.find_element_by_id('project-sidebar').get_attribute('innerHTML'), features="html.parser")
-            #
-            # j = 0
-            # for s in projectSidebar_html.find_all('span'):
-            #     #skip the last 2
-            #     if (j < len(projectSidebar_html.find_all('span')) - 2):
-            #         self.assertIn(s.text,selectedProj.project_category.split(";"))
-            #     if (j == len(projectSidebar_html.find_all('span')) - 2):
-            #         self.assertEqual(s.text,str(selectedProj.student_num))
-            #     if (j == len(projectSidebar_html.find_all('span')) - 1):
-            #         if (appList == None):
-            #             self.assertEqual(s.text,str(0))
-            #         else:
-            #             selectedApps = [x for x in appList if x.project.project_name == project['project_name'] ]
-            #             self.assertEqual(s.text,str(len(selectedApps)))
-            #     j = j + 1
-
-        #input("Press Enter to continue...")
-        time.sleep(5)
+    ### END HELPER FUNCTIONS ###
 
     def test_access_archive_projects(self):
         projCt = random.randint(1, 10)
@@ -156,14 +95,6 @@ class ArchiveTest(StaticLiveServerTestCase):
 
         for i in range(0, projCt):
             projList.append(ProjectFactory(semester=self.semesters[random.randint(1, self.semesterCt)-1]))
-
-        # for i in range(0, random.randint(1, 10)):
-        #     partnerList.append(PartnerProjectInfoFactory(project=projList[random.randint(1, projCt) - 1]))
-
-        # for i in range(0, appCt):
-        #     appList.append(ApplicationFactory(project=projList[random.randint(1, projCt) - 1]))
-
-        # print (partnerList)
 
         self.selenium.get('%s%s' % (self.live_server_url, reverse('index')))
         self.page_validation (projList)
@@ -179,13 +110,8 @@ class ArchiveTest(StaticLiveServerTestCase):
         for i in range(0, projCt):
             projList.append(ProjectFactory(semester=self.semesters[random.randint(1, self.semesterCt)-1]))
 
-        # for i in range(0, random.randint(1, 10)):
-        #     partnerList.append(PartnerProjectInfoFactory(project=projList[random.randint(1, projCt) - 1]))
-
         for i in range(0, appCt):
             appList.append(ApplicationFactory(project=projList[random.randint(1, projCt) - 1]))
 
-        # print (partnerList)
-
         self.selenium.get('%s%s' % (self.live_server_url, reverse('index')))
-        self.page_validation (projList,appList)
+        self.page_validation (projList, appList)
