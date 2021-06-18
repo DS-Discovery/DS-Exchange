ca609419a7ff8af96e7e4c0641b564f84bb6894d
Eunice Chan <ekchan@berkeley.edu>
2021-06-17 19:29:06 -0700

    admin status summary static and template update to incorporate status filter + cosmetic changes

--- static/js/usertools.js
+++ static/js/usertools.js
@@ -1,9 +1,14 @@
 function toggle(id) {
+  let parent = document.getElementById(id);
   let elem = document.getElementById(id+'-drop');
   let curr_display =  window.getComputedStyle(elem).getPropertyValue('display');
   if (curr_display == 'none'){
+    parent.classList.add('open');
+    parent.classList.remove('closed');
     document.getElementById(id+'-drop').style.display = elem.getAttribute("display");
   } else {
+    parent.classList.remove('open');
+    parent.classList.add('closed');
     elem.setAttribute("display", curr_display);
     document.getElementById(id+'-drop').style.display = "none";
   }
@@ -24,6 +29,7 @@ function toggle_select(select) {
 function add_onclick(id) {
   let elem = document.getElementById(id);
   elem.classList.add('toggle');
+  elem.classList.add('open');
   elem.onclick = function() { toggle(this.id) };
 }
 
@@ -32,5 +38,5 @@ function select_toggle_onclick(id) {
   let select_mode = elem.classList.contains('select')? true : false;
   elem.onclick = function() { toggle_select( select_mode) };
 }
-['tools', 'group', 'semester', 'filter', 'export'].forEach(add_onclick);
+['tools', 'group', 'display', 'semester', 'filter', 'export'].forEach(add_onclick);
 ['showSelect', 'showDeselect'].forEach(select_toggle_onclick);
