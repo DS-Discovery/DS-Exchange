6e6f2417417f4222574448c7737a8b12b01114c5
Prachi Deo <prachisdeo@berkeley.edu>
2021-11-02 11:42:46 -0700

    trying use_x_forwarded_host

--- discovery/settings.py
+++ discovery/settings.py
@@ -15,6 +15,7 @@ import os
 import yaml
 
 SECURE_SSL_REDIRECT = True
+USE_X_FORWARDED_HOST = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
