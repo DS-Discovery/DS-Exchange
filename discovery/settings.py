9fe32bcb01e2a077b24c817eb4d01df0521d61d9
Prachi Deo <prachisdeo@berkeley.edu>
2021-12-10 17:37:01 -0800

    trying social_auth_redirect_is_https

--- discovery/settings.py
+++ discovery/settings.py
@@ -16,6 +16,7 @@ import yaml
 
 #SECURE_SSL_REDIRECT = True
 #USE_X_FORWARDED_HOST = True
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
